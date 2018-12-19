4fc1b6fb512edb77fff1b1a4f284f4842d2c10ee
unknown <842159272@qq.com>
2018-12-19 08:38:45 +0800

    完成了实验三的透视变换

--- opencvTest3_5.py
+++ opencvTest3_5.py
@@ -1,31 +1,45 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('photo\\mogu.png')
+img = cv2.imread('photo\\shudu.png')
 rows, cols, ch = img.shape
+y1, x1 = 0, 0
+y2, x2 = 0, 0
+y3, x3 = 0, 0
+y4, x4 = 0, 0
 
-# 当鼠标按下时变为True
-# 如果mode 为true 绘制矩形。按下'm' 变成绘制曲线。
 # 创建回调函数
 def select_point(event, x, y, flags, param):
-    # 当按下左键是返回起始位置坐标
+    global y1, x1, y2, x2, y3, x3, y4, x4
     if event == cv2.EVENT_LBUTTONDOWN:
+        if y1 == 0:
+            y1, x1 = y, x
+        elif y2 == 0:
+            y2, x2 = y, x
+            cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255))
+        elif y3 == 0:
+            y3, x3 = y, x
+            cv2.line(img, (x1, y1), (x3, y3), (0, 0, 255))
+        elif y4 == 0:
+            y4, x4 = y, x
+            cv2.line(img, (x3, y3), (x4, y4), (0, 0, 255))
+            cv2.line(img, (x2, y2), (x4, y4), (0, 0, 255))
 
-
-
-        cv2.imshow('Output', dst)
-    # 当鼠标左键按下并移动是绘制图形。event 可以查看移动，flag 查看是否按下
 
 cv2.namedWindow('image')
-cv2.setMouseCallback('input', select_point)
-cv2.imshow("image", img)
+cv2.setMouseCallback('image', select_point)
+while True:
+    cv2.imshow("image", img)
+    if y4 != 0:
+        pts1 = np.float32([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
+        pts2 = np.float32([[0, 0], [300, 0], [0, 300], [300, 300]])
 
-while(1):
-    pts1 = np.float32([[y1, x1], [y2, x2], [y3, x3], [y4, x4]])
-    pts2 = np.float32([[0, 0], [300, 0], [0, 300], [300, 300]])
-    M = cv2.getPerspectiveTransform(pts1, pts2)
-    dst = cv2.warpPerspective(img, M, (cols, rows))
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+        M = cv2.getPerspectiveTransform(pts1, pts2)
+        dst = cv2.warpPerspective(img, M, (300, 300))
+        cv2.imshow('Output', dst)
+    if cv2.waitKey(20) & 0xff == 27:
         break
-
 cv2.destroyAllWindows()
+
+
+
